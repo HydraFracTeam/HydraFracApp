b384c68e5a8a5af606ea3589a8179480483bb3e3
K4dZuO <135659482+K4dZuO@users.noreply.github.com>
2026-01-21 18:00:47 +0500

    Refactor/remove test and unused code all unused (#14)
    * refactor: remove unused code from helpers folder
    
    * refactor: partional clear from helpers
    
    * refactor: remove unused files that created unused funcs
    
    * refactor: remove unused from ml_methods

--- helpers/dimensionless/filtration/__init__.py
+++ helpers/dimensionless/filtration/__init__.py
@@ -11,23 +11,18 @@ from .utils import (
     detect_log_scale,
     select_filter_method,
     fill_missing_values,
-    remove_outliers
 )
 
 # Опциональный импорт ML-денойзера
 try:
-    from .denoise import MLDenoiser, apply_ml_denoising
     __all__ = [
         'SignalFilters',
         'PhysicsConstraints',
-        'MLDenoiser',
-        'apply_ml_denoising',
         'compute_snr',
         'compute_oscillation_score',
         'detect_log_scale',
         'select_filter_method',
         'fill_missing_values',
-        'remove_outliers'
     ]
 except ImportError:
     __all__ = [
@@ -38,6 +33,5 @@ except ImportError:
         'detect_log_scale',
         'select_filter_method',
         'fill_missing_values',
-        'remove_outliers'
     ]
 
